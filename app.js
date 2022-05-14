64b7edd2180cca31946c8fbde79557a04e6ec07a
NehadAwad <nehadakash622@gmail.com>
2022-05-15 00:12:22 +0600

    add funtion added

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const fs = require('fs')
 const validator = require('validator')
 const name = require('./utils')
+const yargs = require('yargs')
 
+// console.log(validator.isEmail('asdf@gmail.com'))
 
-console.log(validator.isEmail('asdf@gmail.com'))
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('adding a new note')
+    }
+})
 
+console.log(yargs.argv)
