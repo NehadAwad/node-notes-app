e8566276e95dd886bf1bf901a7fba0435ac60b87
NehadAwad <nehadakash622@gmail.com>
2022-05-17 23:00:33 +0600

    remove cmd added

--- app.js
+++ app.js
@@ -1,54 +1,61 @@
-const fs = require('fs')
-const validator = require('validator')
-const notes = require('./notes.js')
 const yargs = require('yargs')
+const notes = require('./notes.js')
 
+// Customize yargs version
+yargs.version('1.1.0') 
 
-//create add command
+// Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
     builder: {
         title: {
-            describe: 'note title',
+            describe: 'Note title',
             demandOption: true,
             type: 'string'
         },
         body: {
-            describe: 'body',
+            describe: 'Note body',
             demandOption: true,
             type: 'string'
         }
     },
-    handler: function(argv){
+    handler: function (argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
 
-//remove note commad
+// Create remove command
 yargs.command({
     command: 'remove',
-    describe: 'Remove a new note',
-    handler: function(){
-        console.log('removing a new note')
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
     }
 })
 
-//show list command
+// Create list command
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function(){
-        console.log('your note lists')
+    handler: function () {
+        console.log('Listing out all notes')
     }
 })
 
-//read a note command
+// Create read command
 yargs.command({
     command: 'read',
-    describe: 'read a note',
-    handler: function(){
-        console.log('read a note')
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,27 +1,58 @@
 const fs = require('fs')
 
-const getnotes = function(){
-    return "your notes"
+const getNotes = function () {
+    return 'Your notes...'
 }
 
-const addNote = function(title, body){
+const addNote = function (title, body) {
     const notes = loadNotes()
-    console.log(notes)
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
 }
 
-const loadNotes = function(){
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
 
-    try{
+    if (notes.length > notesToKeep.length) {
+        console.log('Note removed!')
+        saveNotes(notesToKeep)
+    } else {
+        console.log('No note found!')
+    }    
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function () {
+    try {
         const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString(dataBuffer)
+        const dataJSON = dataBuffer.toString()
         return JSON.parse(dataJSON)
-    }catch(e){
+    } catch (e) {
         return []
     }
-   
 }
 
 module.exports = {
-    getnotes: getnotes,
-    addNote: addNote
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
 }
