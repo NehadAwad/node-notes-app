80460577894dde15494a9c8e586b71ab64df0285
NehadAwad <nehadakash622@gmail.com>
2022-05-17 23:08:15 +0600

    read and list cmd added

--- app.js
+++ app.js
@@ -41,12 +41,13 @@ yargs.command({
     }
 })
 
+
 // Create list command
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function () {
-        console.log('Listing out all notes')
+    handler() {
+        notes.listNotes()
     }
 })
 
@@ -54,9 +55,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
-        console.log('Reading a note')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
     }
 })
-
 yargs.parse()
